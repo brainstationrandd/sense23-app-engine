d3acf8c5d9eead2ad4a5af52f14d46d9b7235b64
Abu Sayed (AEM Frontend & UI) <abu.sayed@abbvie.com>
2022-11-18 17:49:45 +0600

    form controls updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,11 @@ import Typography from "@component/Typography";
 import Icon from "@component/icon/Icon";
 import CustomModal from '../components/custom-modal/index';
 import { useState } from 'react';
+import TextField from '../components/text-field/TextField';
+import Radio from '../components/radio/Radio';
+import Box from "@component/Box";
+import CheckBox from "./CheckBox";
+import Slider from "../components/slider/Slider";
 var Twitter = require('twitter');
 
 const IndexPage = () => {
@@ -47,6 +52,66 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
         </FlexBox>
 
         <CustomModal size isModalOpen={isOpenmodal} handleModalClose={handleModalClose}>
+          <Typography>
+            <h2>Form Field</h2>
+          </Typography>
+
+          <Box mb={3}>
+            <Radio label={"Option 1"} name="radio1" value={1} mb={2} checked />
+            <Radio label={"Option 2"} name="radio1" value={2} mb={2} />
+          </Box>
+
+          <Grid container>
+              <FlexBox width={"100%"} justifyContent={"space-between"}>
+                <Box mb={3}>
+                  <CheckBox label="Check 1" mb={2} />
+                  <CheckBox label="Check 2" mb={2} />
+                </Box>
+                <Box ml={'auto'}>
+                  <Button m={3} borderRadius={20}  bg={'#D23F57'} p={2}>
+                    <Typography color={"#ffffff"}>
+                      <Icon>user</Icon>
+                    </Typography>
+                  </Button>
+
+                  <FlexBox >
+                    <Button m={3} borderRadius={5}  bg={'#D23F57'} px={4}>
+                      <Typography color={"#ffffff"}>Button Small</Typography>
+                    </Button>
+
+                    <Button m={3} borderRadius={20}  bg={'#D23F57'} px={2}>
+                      <Typography color={"#ffffff"}>
+                        <Icon>plus</Icon>
+                      </Typography>
+                    </Button>
+
+                    <Button size="large" m={3} borderRadius={30}  bg={'#D23F57'} py={3} px="3">
+                      <Typography color={"#ffffff"}>
+                        <Icon>plus</Icon>
+                      </Typography>
+                    </Button>
+                  </FlexBox>
+                </Box>
+              </FlexBox>
+             
+
+          </Grid>
+
+          <Grid container width="100%">
+            <Grid item lg={6} pr={2}>
+              <FlexBox justifyContent="flex-end" >
+                <p>$0 - $2,500</p>
+              </FlexBox>
+              <Slider width="100%" min="0" max="2500" mb={3}/>
+            </Grid>
+            
+          </Grid>
+
+          
+          
+          
+
+          <TextField label="Name" placeholder="Enrter your name" fullwidth />
           <Typography>
             <h4>Modal content here</h4>
             <p><strong>Size should be:</strong> fluid, xl, lg, md, sm</p>
